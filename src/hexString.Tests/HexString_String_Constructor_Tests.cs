e290d0629979ee5d9e905a188244b3f046add66d
sl4gr00m <sl4gr00m@gmail.com>
2017-06-08 07:07:50 -0300

    Add string constructor to HexString type.
    Adds a string constructor to the HexString type along with byte[] and
    string members to store each representation.

---
+++ src/hexString.Tests/HexString_String_Constructor_Tests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace hexString.Tests {
+
+    [TestClass]
+    public class HexString_String_Constructor_Tests {
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Throws_ArgumentNullException_For_Null_String() {
+            new HexString(null as string);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Throws_ArgumentException_For_Invalid_HexString() {
+            new HexString("Invalid hex string.");
+        }
+
+        [TestMethod]
+        public void Accepts_Valid_HexString() {
+            new HexString("000102030405060708090a0b0c0d0e0f");
+        }
+    }
+}
---
+++ src/hexString/HexString.Static.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+namespace hexString {
+
+    public partial class HexString {
+
+        private static readonly char[] HEXADECIMAL_CHARACTERS =
+            "0123456789ABCDEFabcdef".ToCharArray();
+
+        /// <summary>
+        /// Determines whether a character is a hexadecimal character. 
+        /// </summary>
+        /// <param name="character">The character.</param>
+        /// <returns>
+        /// <c>true</c> if <paramref name="character"/> is a hexadecimal character, otherwse
+        /// <c>false</c>.
+        /// </returns>
+        public static bool IsHexadecimalCharacter(char character) {
+            return Array.IndexOf(HEXADECIMAL_CHARACTERS, character) > -1;
+        }
+
+        /// <summary>
+        /// Gets the value represented by a hexadecimal character.
+        /// </summary>
+        /// <param name="character">The character.</param>
+        /// <returns>The value represented by <paramref name="character"/>.</returns>
+        /// <exception cref="ArgumentException">
+        /// <paramref name="character"/> is not a hexadecimal character.
+        /// </exception>
+        public static byte HexadecimalCharacterToByte(char character) {
+            var index = Array.IndexOf(HEXADECIMAL_CHARACTERS, char.ToUpper(character));
+            if (index == -1) {
+                throw new ArgumentException("Must be a valid hexadecimal character.", "character");
+            }
+            return (byte)index;
+        }
+
+        /// <summary>
+        /// Determines whether a string is a valid <see cref="HexString"/>. 
+        /// </summary>
+        /// <param name="stringToValidate">The string.</param>
+        /// <returns>
+        /// <c>true</c> if <paramref name="stringToValidate"/> is a valid <see cref="HexString"/>,
+        /// otherwise <c>false</c>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="stringToValidate"/> is <c>null</c>.
+        /// </exception>
+        public static bool IsValidHexString(string stringToValidate) {
+            if (stringToValidate == null) { throw new ArgumentNullException("stringToValidate"); }
+            if (stringToValidate.Length % 2 != 0) {
+                return false;
+            }
+            for (var i = 0; i < stringToValidate.Length; i += 1) {
+                if (Array.IndexOf(HEXADECIMAL_CHARACTERS, stringToValidate[i]) == -1) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Gets the bytes represented by a hexadecimal string. 
+        /// </summary>
+        /// <param name="hexadecimalString">The string.</param>
+        /// <returns>
+        /// The bytes that <paramref name="hexadecimalString"/> represents.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="hexadecimalString"/> is <c>null</c>.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// <paramref name="hexadecimalString"/> is not a valid <see cref="HexString"/>. 
+        /// </exception> 
+        public static byte[] GetBytes(string hexadecimalString) {
+            if (hexadecimalString == null) {
+                throw new ArgumentNullException("hexadecimalString");
+            }
+            if (!IsValidHexString(hexadecimalString)) {
+                throw new ArgumentException("Must be a valid HexString.", "hexadecimalString");
+            }
+            var bytes = new byte[hexadecimalString.Length / 2];
+            for (var bytesIndex = 0; bytesIndex < bytes.Length; bytesIndex += 1) {
+                var stringIndex = bytesIndex * 2;
+                var highOrderCharacter = hexadecimalString[stringIndex++];
+                var lowOrderCharacter = hexadecimalString[stringIndex];
+                var highOrderValue = HexadecimalCharacterToByte(highOrderCharacter);
+                var lowOrderValue = HexadecimalCharacterToByte(lowOrderCharacter);
+                bytes[bytesIndex] = (byte)((highOrderValue << 4) + lowOrderValue);
+            }
+            return bytes;
+        }
+
+        /// <summary>
+        /// Gets the hexadecimal string that represents a byte array.
+        /// </summary>
+        /// <param name="bytes">The bytes.</param>
+        /// <returns>The hexadecimal string that represents <paramref name="bytes"/>.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="bytes"/> is <c>null</c>.
+        /// </exception>
+        public static string GetString(byte[] bytes) {
+            if (bytes == null) { throw new ArgumentNullException("bytes"); }
+            var chars = new char[bytes.Length * 2];
+            for (var bytesIndex = 0; bytesIndex < bytes.Length; bytesIndex += 1) {
+                var highOrderValue = bytes[bytesIndex] >> 4;
+                var lowOrderValue = bytes[bytesIndex] & 0x0F;
+                chars[bytesIndex * 2] = HEXADECIMAL_CHARACTERS[highOrderValue];
+                chars[bytesIndex * 2 + 1] = HEXADECIMAL_CHARACTERS[lowOrderValue];
+            }
+            return new string(chars);
+        }
+    }
+}
