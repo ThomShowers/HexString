3c20adf072fa299eeb16f0a57f3adcc9db9eca51
sl4gr00m <sl4gr00m@gmail.com>
2017-06-08 22:11:03 -0300

    Add ToBytes() method.

---
+++ src/hexString.Tests/HexString_ToBytes_Tests.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace hexString.Tests {
+
+    [TestClass]
+    public class HexString_ToBytes_Tests {
+
+        [TestMethod]
+        public void Returns_Correct_String_After_Bytes_Constructor() {
+            var expectedBytes = 
+                new byte[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+            var hexString = new HexString(expectedBytes);
+            Assert.IsTrue(expectedBytes.SequenceEqual(hexString.ToBytes()));
+        }
+
+        [TestMethod]
+        public void Returns_Correct_String_After_String_Constructor() {
+            var expectedBytes =
+                new byte[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+            var hexString = new HexString("000102030405060708090a0b0c0d0e0f");
+            Assert.IsTrue(expectedBytes.SequenceEqual(hexString.ToBytes()));
+        }
+    }
+}
