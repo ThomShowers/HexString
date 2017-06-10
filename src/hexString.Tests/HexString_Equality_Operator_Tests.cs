ee4082824b6790d67137b0a72526c534375bc67a
sl4gr00m <sl4gr00m@gmail.com>
2017-06-10 09:07:05 -0300

    Implement value equality comparisons.

---
+++ src/hexString.Tests/HexString_Equality_Operator_Tests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace hexString.Tests {
+
+    [TestClass]
+    public class HexString_Equality_Operator_Tests {
+
+        [TestMethod]
+        public void Equality_Operator_Returns_False_For_Inequal_Valued_Instances() {
+            var hexStringA = new HexString("face");
+            var hexStringB = new HexString("fade");
+            Assert.IsFalse(hexStringA == hexStringB);
+        }
+
+        [TestMethod]
+        public void Equality_Operator_Returns_True_For_Equal_Valued_Instances() {
+            var hexStringA = new HexString("face");
+            var hexStringB = new HexString("face");
+            Assert.IsTrue(hexStringA == hexStringB);
+        }
+
+        [TestMethod]
+        public void Equality_Operator_Equates_Two_Nulls() {
+            Assert.IsTrue((null as HexString) == (null as HexString));
+        }
+
+        [TestMethod]
+        public void Equality_Operator_Handles_Null_Left() {
+            var hexString = new HexString("face");
+            Assert.IsFalse((null as HexString) == hexString);
+        }
+
+        [TestMethod]
+        public void Equality_Operator_Handles_Null_Right() {
+            var hexString = new HexString("face");
+            Assert.IsFalse(hexString == (null as HexString));
+        }
+    }
+}
---
+++ src/hexString.Tests/HexString_Equals_Tests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace hexString.Tests {
+
+    [TestClass]
+    public class HexString_Equals_Tests {
+
+        [TestMethod]
+        public void Equals_Returns_False_For_Null_Object() {
+            var hexString = new HexString("");
+            Assert.IsFalse(hexString.Equals(null));
+        }
+
+        [TestMethod]
+        public void Equals_Returns_False_For_Non_HexString_Object() {
+            var hexString = new HexString("face");
+            Assert.IsFalse(hexString.Equals(1234));
+        }
+
+        [TestMethod]
+        public void Equals_Returns_False_For_Null_HexString() {
+            var hexString = new HexString("face");
+            Assert.IsFalse(hexString.Equals(null as HexString));
+        }
+
+        [TestMethod]
+        public void Equals_Returns_False_For_Inequal_HexString() {
+            var hexString = new HexString("face");
+            Assert.IsFalse(hexString.Equals(new HexString("fade")));
+        }
+
+        [TestMethod]
+        public void Equals_Returns_True_For_Equal_HexString() {
+            var hexString = new HexString("face");
+            Assert.IsTrue(hexString.Equals(new HexString("face")));
+        }
+    }
+}
